2a9be4fd537ccc74d42f0a52a2e1f794b1f45a9b
justin <justin_yih@hotmail.com>
2020-01-22 11:45:29 -0800

    update to master branch 1.1

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,50 @@
-const express   = require('express'),
-      passport  = require('passport'),
-      path      = require('path'),
-      router    = express.Router();
+const express = require("express"),
+	passport = require("passport"),
+	path = require("path"),
+	router = express.Router();
 
-//import passport module 
-require('./passport')(passport);
+//import passport module
+require("./passport")(passport);
 
 //Get homepage
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../login.html'));
-})
-    //Update home page with converted 
+router.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "../home.html"));
+});
+//Update home page with converted
 
 //user login
-router.route('/login')
-    //render to user login page
-    .get((req, res) => {
-        res.sendFile(path.join(__dirname, '../login.html'));
-    })
-    //handle user login authentication request, 
-    //if failed, redirect user to login page. Otherwise redirect to dashboard
-    .post(passport.authenticate('local-login', 
-        { 
-            failureRedirect: '/login',
-            successRedirect: '/users/protected',
-            failureFlash: 'invalid username or password.'
-        })
-    );
-    
+router
+	.route("/login")
+	//render to user login page
+	.get((req, res, next) => {
+		// console.log('test4');
+		res.sendFile(path.join(__dirname, "../login.html"));
+	})
+	//handle user login authentication request,
+	//if failed, redirect user to login page. Otherwise redirect to dashboard
+	.post(
+		passport.authenticate("local-login", {
+			successRedirect: "/users/protected",
+			failureRedirect: "/login",
+			failureFlash: true
+		})
+	);
+
 //user register
-router.route('/register')
-    //reander to user registration page
-    .get((req, res) => {
-        res.render('register', {error:false});       
-    })
-    //handle user registeration request
-    //if failed, redirect user to register page. Otherwise redirect to dashboard
-    .post(passport.authenticate('local-signup', 
-        { 
-            failureRedirect: '/register',
-            successRedirect: '/users/protected',
-            failureFlash: 'invalid username or password.'
-        })
-    );
-              
+router
+	.route("/register")
+	//reander to user registration page
+	.get((req, res, next) => {
+		res.sendFile(path.join(__dirname, "../register.html"));
+	})
+	//handle user registeration request
+	//if failed, redirect user to register page. Otherwise redirect to dashboard
+	.post(
+		passport.authenticate("local-signup", {
+			successRedirect: "/users/protected",
+			failureRedirect: "/register",
+			failureFlash: true
+		})
+	);
+
 module.exports = router;
--- routes/passport.js
+++ routes/passport.js
@@ -1,69 +1,91 @@
-
-const LocalStrategy   = require('passport-local').Strategy,
-      UserDB          = require('../src/data/UserDB'),
-      User            = new UserDB();
-
+const LocalStrategy = require("passport-local").Strategy,
+	UserDB = require("../src/data/UserDB"),
+	User = new UserDB();
 
 module.exports = function(passport) {
-    //serializing the user => store user.id into the session
-    passport.serializeUser(function(user, done) {
-        done(null, user.id);
-    });
-    
-    //deserializing the user => retrieve user instance from user database by the user.id stored in the session
-    passport.deserializeUser(function(id, done) {
-        User.findUserById(id, function(err, user) {
-            done(err, user);
-        });
-    });    
+	//serializing the user => store user.id into the session
+	passport.serializeUser(function(user, done) {
+		done(null, user.id);
+	});
 
-    // login authentication configuration => use local strategy
-    passport.use('local-login', new LocalStrategy(
-            {
-                passReqToCallback : true
-            },
-            function(req, email, password, done ){
-                // Find the user by username. If there is no user with the given username & password
-                // set the user to `false` to indicate failure. Otherwise, return the user
-                User.findUserByEmail(
-                    email, 
-                    password,
-                    function( err, user )
-                    {
-                        if ( err ) 	 return done( err );
-                        if ( !user ) {return done( null, false );}
-                        else {return done( null, user);}
-                    }
-                );
-            }
-        ));
+	//deserializing the user => retrieve user instance from user database by the user.id stored in the session
+	passport.deserializeUser(function(id, done) {
+		User.findUserById(id, function(err, user) {
+			done(err, user);
+		});
+	});
 
-    // signup authentication configuration => use local strategy    
-    passport.use('local-signup', new LocalStrategy({
-            usernameField : 'email',
-            passwordField : 'password',
-            passReqToCallback : true 
-        },
-        function(req, email, password, done) {
-            // const password_confirm = ;
-            // console.log(username, password, password_confirm, email);
+	// login authentication configuration => use local strategy
+	passport.use(
+		"local-login",
+		new LocalStrategy(
+			{
+				usernameField: "email",
+				passwordField: "password",
+				passReqToCallback: true
+			},
+			function(req, email, password, done) {
+				// Find the user by username. If there is no user with the given username & password
+				// set the user to `false` to indicate failure. Otherwise, return the user
+				console.log("test1");
+				User.findUserByEmail(email, password, function(err, user) {
+					if (err) return done(err);
+					if (!user) {
+						return done(null, false);
+					} else {
+						return done(null, user);
+					}
+				});
+			}
+		)
+	);
 
-            // console.log(password === password_confirm );
-            // verify double-input of passwords matching. If failed, set the user to `false` to indicate failure.
-            if (password !== req.body.password_confirm) {
-                return done(null, false, req.flash('signupMessage', 'Password doesn\'t match.'));}
-            // verify the availibility of the username and email. If failed, set the user to `false` to indicate failure.
-            if (User.users.find((user) => user.email === email)) {
-               
-                return done(null, false, req.flash('signupMessage', 'Username or email has already been taken.'));
-            } else { 
-                // create a new user and insert it into the database, then return the user instance.
-                // passport local strategy only supports usernameField(email) and passwordField, 
-                // so the firstname and lastname have to be pulled out from req.body object
-                const user =  User.createUser(req.body.firstname, req.body.lastname, password, email);
-                console.log(User.users);
-                return done(null, user, req.flash('signupMessage', 'User registered successfully.'));
-            }
-        }
-    ));
+	// signup authentication configuration => use local strategy
+	passport.use(
+		"local-signup",
+		new LocalStrategy(
+			{
+				usernameField: "email",
+				passwordField: "password",
+				passReqToCallback: true
+			},
+			function(req, email, password, done) {
+				// verify double-input of passwords matching. If failed, set the user to `false` to indicate failure.
+				if (password !== req.body.password_confirm) {
+					return done(
+						null,
+						false,
+						req.flash("signupMessage", "Password doesn't match.")
+					);
+				}
+				// verify the availibility of the username and email. If failed, set the user to `false` to indicate failure.
+				if (User.users.find((user) => user.email === email)) {
+					return done(
+						null,
+						false,
+						req.flash(
+							"signupMessage",
+							"Username or email has already been taken."
+						)
+					);
+				} else {
+					// create a new user and insert it into the database, then return the user instance.
+					// passport local strategy only supports usernameField(email) and passwordField,
+					// so the firstname and lastname have to be pulled out from req.body object
+					const user = User.createUser(
+						req.body.firstname,
+						req.body.lastname,
+						password,
+						email
+					);
+					console.log(User.users);
+					return done(
+						null,
+						user,
+						req.flash("signupMessage", "User registered successfully.")
+					);
+				}
+			}
+		)
+	);
 };
